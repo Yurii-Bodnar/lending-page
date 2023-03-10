abfcf452c64246aae004e1f93a85597422d59793
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2023-03-10 11:41:14 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  //   console.log('hello');
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', background: 'black' }}
+    >
+      App
+    </div>
+  );
+};
+
+export default App;
